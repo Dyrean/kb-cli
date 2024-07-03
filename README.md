36065b9e220ed626b5686c70976dc7ec010ae2d5
Dyrean <fcanbeyy@gmail.com>
2024-07-03 22:13:04 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+[!CAUTION]
+This is still in development.
+
+Kanban Board in Terminal with Bubble Tea
+
+This project will help me to learn charm ecosystem (https://charm.sh/)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type status int
+
+const (
+	todo status = iota
+	inProgress
+	done
+)
+
+type Task struct {
+	status      status
+	title       string
+	description string
+}
+
+// Implement the list.Item interface
+
+func (t Task) FilterValue() string {
+	return t.title
+}
+
+func (t Task) Title() string {
+	return t.title
+}
+
+func (t Task) Description() string {
+	return t.description
+}
+
+type Model struct {
+	list list.Model
+	err  error
+}
+
+func New() *Model {
+	return &Model{}
+}
+
+// TODO: Call initList on tea.WindowSizeMsg
+func (m *Model) initList(width, height int) {
+	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
+	m.list.Title = "To Do"
+	m.list.SetItems([]list.Item{
+		Task{status: todo, title: "buy milk", description: "strawberry milk"},
+		Task{status: todo, title: "drink water", description: "3L"},
+		Task{status: todo, title: "cook food", description: "chicken salad"},
+	})
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.initList(msg.Width, msg.Height)
+	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m Model) View() string {
+	return m.list.View()
+}
+
+func main() {
+	p := tea.NewProgram(New())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("There's been an error: %v", err)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
